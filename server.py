5f4b9a4e244e3e1a83c60589e19ca43e37b77226
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-15 18:27:22 -0500

    SavePoint

--- server.py
+++ server.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, session, redirect, request
 app = Flask(__name__)
 app.secret_key = "Could be whatever I want. Really."
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/')
+
+
+
 @app.route('/')
 def root():
     return render_template("index.html")

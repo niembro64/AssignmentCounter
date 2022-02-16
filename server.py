656abeecf7410ec2085e7bf131b930dcd77388b0
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-15 20:03:57 -0500

    Working Minus Reset

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
+from contextlib import nullcontext
 from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 
+
 @app.route('/')
 def root():
+    if session['sc'] < 1:
+        session['sc'] = 0
+    else:
+        session['sc'] = session['sc'] + 1
     return render_template("index.html")
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    print("Got Post Info")
+    # print("Got Post Info")
     # Here we add two properties to session to store the name and email
+    if session['sc'] < 1:
+        session['sc'] = 0
+    else:
+        session['sc'] = session['sc'] + 1
     session['username'] = request.form['name']
     session['useremail'] = request.form['email']
-    return redirect('/show')
+    return redirect('/#')
+    # return redirect('/show')
 
 @app.route('/show')
 def show_user():

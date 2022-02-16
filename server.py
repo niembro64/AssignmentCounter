9142a52596802acfb273ce5f0e6f88224fceef63
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-15 19:20:35 -0500

    Working

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
-app.secret_key = "Could be whatever I want. Really."
+app.secret_key = "secret key"
+
+
+@app.route('/')
+def root():
+    return render_template("index.html")
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -9,13 +14,13 @@ def create_user():
     # Here we add two properties to session to store the name and email
     session['username'] = request.form['name']
     session['useremail'] = request.form['email']
-    return redirect('/')
+    return redirect("/#")
+    # return redirect('/show')
 
+@app.route('/show')
+def show_user():
+    return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
 
-
-@app.route('/')
-def root():
-    return render_template("index.html")
 
 
 if __name__ == "__main__":
